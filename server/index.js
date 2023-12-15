0b06c19cad6618965b92c3b430fc25d850268615
Sai Vaddemani <svaddemani@innominds.com>
2023-12-15 14:31:58 +0530

    added health api

--- server/index.js
+++ server/index.js
@@ -3,42 +3,39 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 
-
 const PORT = process.env.PORT || 8080;
 const indexPath = path.resolve(__dirname, "..", "build", "index.html");
 
 // static resources should just be served as they are
-app.use(express.static(
-  path.resolve(__dirname, "..", "build"),
-  { maxAge: "30d" },
-));
+app.use(
+  express.static(path.resolve(__dirname, "..", "build"), { maxAge: "30d" })
+);
 
 const defaultPortalDetails = {
   title: "Bullitt Satilite Messenger",
   thumbnail: "/home_illustration.jpg",
-  description: "Bullit satelite tracking"
-}
+  description: "Bullit satelite tracking",
+};
 
 const getPostData = async () => {
-    const imgs =[
-        "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
-        "https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s",
-        "https://fastly.picsum.photos/id/22/4434/3729.jpg?hmac=fjZdkSMZJNFgsoDh8Qo5zdA_nSGUAWvKLyyqmEt2xs0"
-    ]
-  try{
-    const randImgNumber = Math.round((Math.random() * imgs.length));
+  const imgs = [
+    "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+    "https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s",
+    "https://fastly.picsum.photos/id/22/4434/3729.jpg?hmac=fjZdkSMZJNFgsoDh8Qo5zdA_nSGUAWvKLyyqmEt2xs0",
+  ];
+  try {
+    const randImgNumber = Math.round(Math.random() * imgs.length);
     const sessionInfo = {
       title: `Sai's - Tracking Session #${randImgNumber} `,
       description: "",
-      thumbnail: imgs[randImgNumber]
-    }
+      thumbnail: imgs[randImgNumber],
+    };
 
-    return sessionInfo
-  }
-  catch(err){
+    return sessionInfo;
+  } catch (err) {
     return defaultPortalDetails;
   }
-}
+};
 
 // Middleware to extract the full URL from the request
 app.use((req, res, next) => {
@@ -56,7 +53,7 @@ const replaceMetaTags = (htmlData, data, baseUrl, originalUrl) => {
     .replace("__META_TWITTER_TITLE__", data.title)
     .replace("__META_TWITTER_DESCRIPTION__", data.description)
     .replace("__META_TWITTER_IMAGE__", baseUrl + data.thumbnail);
-}
+};
 
 // here we serve the index.html page
 app.get("/livetracking", async (req, res) => {
@@ -66,23 +63,34 @@ app.get("/livetracking", async (req, res) => {
   fs.readFile(indexPath, "utf8", (err, htmlData) => {
     if (err) {
       console.error("Error during file reading", err);
-      return res.status(404).end()
+      return res.status(404).end();
     }
     // get post info
     // const postId = req.query;
-    getPostData().then((sessionData => {
-      // inject meta updated tags
-      htmlData = replaceMetaTags(htmlData, sessionData, baseUrl, originalUrl);
-      return res.send(htmlData);
-
-    })).catch((err) => {
-      // inject meta tags
-      htmlData = replaceMetaTags(htmlData, defaultPortalDetails, baseUrl, originalUrl);
-      return res.send(htmlData);
-    })
+    getPostData()
+      .then((sessionData) => {
+        // inject meta updated tags
+        htmlData = replaceMetaTags(htmlData, sessionData, baseUrl, originalUrl);
+        return res.send(htmlData);
+      })
+      .catch((err) => {
+        // inject meta tags
+        htmlData = replaceMetaTags(
+          htmlData,
+          defaultPortalDetails,
+          baseUrl,
+          originalUrl
+        );
+        return res.send(htmlData);
+      });
   });
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send({
+    message: "App is running",
+  });
+});
 
 app.get("/*", (req, res) => {
   const baseUrl = req.fullUrl;
@@ -97,7 +105,6 @@ app.get("/*", (req, res) => {
     return res.send(htmlData);
   });
 });
-
 
 // listening...
 app.listen(PORT, (error) => {
